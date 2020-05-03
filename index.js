967dd12187b8682f365430a229373f1f2562df46
Yadhuse-PC <aryaroy.sailam99@gmail.com>
2020-05-03 07:59:00 +0530

    uploading files

---
+++ index.js
@@ -0,0 +1,25 @@
+function ajax(){
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+
+        if(this.readyState==4&&this.status==200){
+            var response = JSON.parse(this.responseText);
+            var Jlist = response.list;
+            var output ="";
+            for(var i=0;i<Jlist.length;i++){
+                output += "<tr>";
+                output += "<th>" + Jlist[i].no +"</th>";
+                output += "<th>" + Jlist[i].item +"</th>";
+                output += "<th>" + Jlist[i].quantity +"</th>";
+                output += "</tr>";
+
+            }
+            document.getElementById("demo").innerHTML = output;
+        }
+
+    }
+
+xhttp.open("GET","list.json",true);
+xhttp.send();
+}
